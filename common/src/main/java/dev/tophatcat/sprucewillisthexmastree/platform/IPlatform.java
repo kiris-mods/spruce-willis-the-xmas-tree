a3936abf44cc3327f0e892a89e989ad3f3c74a77
KiriCattus <19393068+KiriCattus@users.noreply.github.com>
2025-12-29 15:23:27 +0000

    Extra work towards 1.21.11

---
+++ common/src/main/java/dev/tophatcat/sprucewillisthexmastree/platform/IPlatform.java
@@ -0,0 +1,35 @@
+package dev.tophatcat.sprucewillisthexmastree.platform;
+
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.Mob;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.SpawnEggItem;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.entity.BlockEntityType;
+
+import java.util.function.Supplier;
+
+public interface IPlatform {
+
+    String getPlatformName();
+    boolean isModLoaded(String modId);
+    boolean isDevelopmentEnvironment();
+    default String getEnvironmentName() {
+        return isDevelopmentEnvironment() ? "development" : "production";
+    }
+
+    <T extends BlockEntity> Supplier<BlockEntityType<T>> registerBlockEntity(
+        String id, Supplier<BlockEntityType<T>> blockEntityType);
+    <T extends Block> Supplier<T> registerBlock(String id, Supplier<T> block);
+    <T extends Entity> Supplier<EntityType<T>> registerEntity(String id, Supplier<EntityType<T>> entity);
+    <T extends Item> Supplier<T> registerItem(String id, Supplier<T> item);
+    <T extends SoundEvent> Supplier<T> registerSound(String id, Supplier<T> sound);
+    <T extends CreativeModeTab> Supplier<T> registerCreativeModeTab(String id, Supplier<T> tab);
+    <E extends Mob> Supplier<SpawnEggItem> makeSpawnEgg(
+        Item.Properties itemProperties, Supplier<EntityType<E>> entityType);
+    CreativeModeTab.Builder newCreativeTabBuilder();
+}
--- fabric/src/main/java/dev/tophatcat/sprucewillisthexmastree/platform/PlatformFabric.java
+++ fabric/src/main/java/dev/tophatcat/sprucewillisthexmastree/platform/PlatformFabric.java
@@ -17,7 +17,12 @@
 package dev.tophatcat.sprucewillisthexmastree.platform;
 
 import dev.tophatcat.sprucewillisthexmastree.WillisCommon;
-import dev.tophatcat.sprucewillisthexmastree.WillisNeo;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
+import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.core.Holder;
+import net.minecraft.core.Registry;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.resources.Identifier;
 import net.minecraft.sounds.SoundEvent;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
@@ -28,57 +33,55 @@ import net.minecraft.world.item.SpawnEggItem;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityType;
-import net.neoforged.fml.ModList;
-import net.neoforged.fml.loading.FMLEnvironment;
 
 import java.util.function.Supplier;
 
-public class PlatformHelperNeo implements IPlatform {
+public class PlatformFabric implements IPlatform {
 
     @Override
     public String getPlatformName() {
-        return "NeoForge";
+        return "Fabric";
     }
 
     @Override
     public boolean isModLoaded(String modId) {
-        return ModList.get().isLoaded(WillisCommon.MOD_ID);
+        return FabricLoader.getInstance().isModLoaded(WillisCommon.MOD_ID);
     }
 
     @Override
     public boolean isDevelopmentEnvironment() {
-        return !FMLEnvironment.isProduction();
+        return FabricLoader.getInstance().isDevelopmentEnvironment();
     }
 
     @Override
     public <T extends BlockEntity> Supplier<BlockEntityType<T>> registerBlockEntity(
         String id, Supplier<BlockEntityType<T>> blockEntityType) {
-        return WillisNeo.BLOCK_ENTITIES.register(id, blockEntityType);
+        return registerSupplier(BuiltInRegistries.BLOCK_ENTITY_TYPE, id, blockEntityType);
     }
 
     @Override
     public <T extends Block> Supplier<T> registerBlock(String id, Supplier<T> block) {
-        return WillisNeo.BLOCKS.register(id, block);
+        return registerSupplier(BuiltInRegistries.BLOCK, id, block);
     }
 
     @Override
     public <T extends Entity> Supplier<EntityType<T>> registerEntity(String id, Supplier<EntityType<T>> entity) {
-        return WillisNeo.ENTITIES.register(id, entity);
+        return registerSupplier(BuiltInRegistries.ENTITY_TYPE, id, entity);
     }
 
     @Override
     public <T extends Item> Supplier<T> registerItem(String id, Supplier<T> item) {
-        return WillisNeo.ITEMS.register(id, item);
+        return registerSupplier(BuiltInRegistries.ITEM, id, item);
     }
 
     @Override
     public <T extends SoundEvent> Supplier<T> registerSound(String id, Supplier<T> sound) {
-        return WillisNeo.SOUND_EVENTS.register(id, sound);
+        return registerSupplier(BuiltInRegistries.SOUND_EVENT, id, sound);
     }
 
     @Override
     public <T extends CreativeModeTab> Supplier<T> registerCreativeModeTab(String id, Supplier<T> tab) {
-        return WillisNeo.CREATIVE_TABS.register(id, tab);
+        return registerSupplier(BuiltInRegistries.CREATIVE_MODE_TAB, id, tab);
     }
 
     @Override
@@ -89,6 +92,21 @@ public class PlatformHelperNeo implements IPlatform {
 
     @Override
     public CreativeModeTab.Builder newCreativeTabBuilder() {
-        return CreativeModeTab.builder();
+        return FabricItemGroup.builder();
     }
+
+    @SuppressWarnings("unchecked")
+    private static <T, R extends Registry<? super T>> Supplier<T> registerSupplier(
+        R registry, String id, Supplier<T> object) {
+        final T registeredObject = Registry.register((Registry<T>) registry, Identifier.fromNamespaceAndPath(
+            WillisCommon.MOD_ID, id), object.get());
+        return () -> registeredObject;
+    }
+
+    @SuppressWarnings("unchecked")
+    private static <T, R extends Registry<? super T>> Holder<T> registerHolder(
+        R registry, String id, Supplier<T> object) {
+        return Registry.registerForHolder((Registry<T>) registry, Identifier.fromNamespaceAndPath(
+            WillisCommon.MOD_ID, id), object.get());
+        }
 }
